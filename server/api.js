6ad1919e64f0b20023a8f58a7e8d48a27dbaeaad
Jim Toth <jim@artby.city>
2023-01-06 15:44:43 -0500

    feat: adds  endpoint

--- server/api.js
+++ server/api.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { isTonSigner } from "./utils/ton/signer.js";
 import { isMassaSigner } from "./utils/massa/signer.js";
-
+import { isDesoSigner } from "./utils/deso/signer.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -42,6 +42,21 @@ app.get("/massa/:pubkey/:message/:signature", async (req, res) => {
     return;
   }
 });
+
+app.get("/deso/:address/:message/:signature", async (req, res) => {
+  try {
+    res.setHeader("Content-Type", "application/json");
+    const { address, message, signature } = req.params;
+
+    const response = await isDesoSigner(address, message, signature);
+    res.send(response);
+    return;
+  } catch (error) {
+    console.log(error);
+    res.send({ result: false, address: null });
+    return;
+  }
+})
 
 app.listen(port, async () => {
   console.log(`listening at PORT:${port}`);
